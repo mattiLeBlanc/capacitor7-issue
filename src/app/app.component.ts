1dec4d65e6fd7010871222356151d66d60f78f1a
Mattijs Spierings <mattijs@earnr.com.au>
2024-06-17 09:35:38 +0800

    store current files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import {MatCardModule} from '@angular/material/card';
+import { TestComponent } from './test/test/test.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  standalone: true,
+  imports: [MatCardModule, TestComponent],
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
 }
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,12 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { appConfig } from './app/app.config';
+import { AppComponent } from './app/app.component';
 
-import { AppModule } from './app/app.module';
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+try {
+  bootstrapApplication(AppComponent, appConfig)
+    .catch((err) => console.error(err));
+
+} catch (error: any) {
+  document.body.innerHTML = '[MAIN]' + error;
+}
