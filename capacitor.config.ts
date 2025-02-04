6dc55af2e68914e234ec85905b1c9723ec006351
Mattijs Spierings <mattijs@earnr.com.au>
2025-02-05 09:04:27 +1100

    update

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'tv-test',
+  webDir: 'dist/tv-test'
+};
+
+export default config;
--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,25 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
-
 try {
   bootstrapApplication(AppComponent, appConfig)
-    .catch((err) => console.error(err));
+    .catch(error => {
+      showErrorLog(error);
+    })
 
 } catch (error: any) {
-  document.body.innerHTML = '[MAIN]' + error;
+  showErrorLog(error);
+}
+
+
+function showErrorLog(error: any) {
+  document.getElementsByTagName('body')[0].innerHTML = `
+      <div style="padding:24px">
+        <h1>Resparke failed to load because of the following error:</h1>
+        <h3>"${error.message}"</h3>
+        <pre style="font-weight: 300; text-wrap: inherit; background-color: #E0F3F3; padding: 16px;">${error.stack.toString()}
+        </pre>
+        <p style="padding: 16px; background-color: #E4C5DC;">If you get this error, please take a picture or screenshot and report it at https://www.resparke.com/support/</p>
+      </div>
+      `
 }
